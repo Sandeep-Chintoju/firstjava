d7804b357bcd9bf51b43e9d6aed83c88a36ec137
Sandeep-Chintoju <50543277+Sandeep-Chintoju@users.noreply.github.com>
2019-05-24 21:48:05 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,2 +1,9 @@
-java -version
-dir
+
+/** 
+ * The HelloWorldApp class implements an application that simply prints "Hello World" to standard output.
+ */
+public class HelloWorld {
+  public static void main(String[] args) {
+    System.out.println("Hello, World");
+  }
+}
